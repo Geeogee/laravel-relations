85702ff87c08afd33700fd8caa678f76c65d7314
Gioele <gioele.armiento@gmail.com>
2021-06-03 18:49:13 +0200

    Create done

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -2,4 +2,12 @@
     <h2>
         Header
     </h2>
+
+    <a href="{{ route('home') }}">
+        Back to Homepage
+    </a>
+
+    <a href="{{ route('create') }}">
+        Add a new car 
+    </a>
 </header>
---
+++ laravel/resources/views/pages/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.main-layout')
+
+@section('title')
+    <title>
+        Add a new car
+    </title>
+@endsection
+
+@section('main')
+    <main>
+        <div class="container">
+            <form action="{{ route('store') }}" method="POST">
+
+                @csrf
+                @method('POST')
+
+                <div class="wrapper-input">
+                    <label for="name"> Car name: </label>
+                    <input type="text" id="name" name="name">
+                </div>
+                <div class="wrapper-input">
+                    <label for="model"> Car model: </label>
+                    <input type="text" id="model" name="model">
+                </div>
+                <div class="wrapper-input">
+                    <label for="kw"> Car kw: </label>
+                    <input type="number" id="kw" name="kw" min="30">
+                </div>
+                <div class="wrapper-input">
+                    <label for="brand_id"> Brand: </label> 
+                    <select name="brand_id" id="brand_id">
+                        @foreach ($brands as $brand)
+                            <option value="{{ $brand -> id }}">{{ $brand -> name }} ({{ $brand -> nationality }})</option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit">
+                    Create
+                </button>
+            </form>
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+    </main>
+@endsection
