26589beb3e9903e6053deac93d9440f93352e6ee
Gioele <gioele.armiento@gmail.com>
2021-06-03 15:08:56 +0200

    SQLSTATE[23000]: Integrity constraint violation: 1217 Cannot delete or update a parent row: a foreign key constraint fails (SQL: drop table if exists `brands`)

---
+++ laravel/database/migrations/2021_06_03_125738_add_foreign_keys.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cars', function(Blueprint $table) {
+
+            $table 
+                -> foreign('brand_id', 'car_brand')
+                -> references('id')
+                -> on('brands');
+        });
+
+        Schema::table('car_pilot', function(Blueprint $table) {
+
+            $table 
+                -> foreign('car_id', 'car_pilot')
+                -> references('id')
+                -> on('pilots');
+            
+            $table 
+                -> foreign('pilot_id', 'pilot_car')
+                -> references('id')
+                -> on('cars');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
